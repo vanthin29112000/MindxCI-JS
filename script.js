648bfd35d696e0be4b6fa079ec3c770d5e839eb6
vanthin2911 <vanthin1203@gmail.com>
2021-04-20 22:29:16 +0700

    Lesson1

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const navItems = [
     {
-        name : "Item 1"
+        name : "Item 1",
+        children : ["Item 1.1","Item 1.2","Item 1.3"]
     },
     {
-        name : "Item 2"
+        name : "Item 2",
+        children : [
+            "Item 1.1",
+            "Item 1.2",
+            "Item 1.3",
+            "Item 1.1",
+            "Item 1.2",
+            "Item 1.3"]
     },
     {
         name : "Item 3"
@@ -17,9 +25,25 @@ const navItems = [
 ];
 
 window.onload = function() {
+    let i = 0; 
     for(let a of navItems)
     {
         document.getElementsByClassName("menu")[0].innerHTML += 
-        `<span>`+a.name+`</span>`;
+        `<span>`+a.name+`
+        <div class="menu-vertical menu_dropdown">
+        </div>        
+        </span>`;
+        try
+        {
+            for(let b of a.children)
+            {
+            document.getElementsByClassName("menu_dropdown")[i].innerHTML += 
+            `<span>`+b+`</span>`;
+            }
+            i++;
+        }
+        catch{
+            console.log("lỗi");
+        }
     }
 }
